f19153e0aa09e723d36a7513d131684fa5e0f54d
Jevin <therealjevin@gmail.com>
2023-01-11 09:25:49 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(names, name) {
+    return names.filter(n => n.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(names, startingLetters) {
+    return names.filter(n => {
+        let sub = n.substr(0,startingLetters.length).toLowerCase();
+        return sub === startingLetters.toLowerCase();
+    })
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+}
